2382074f550d22425ec0b3a2dbea06b86dacec73
srihas <srihasgupta@gmail.com>
2026-07-24 11:30:08 -0500

    Adjusted window mins and defaults

--- mac-app/Sources/InstallFonts/InstallFontsApp.swift
+++ mac-app/Sources/InstallFonts/InstallFontsApp.swift
@@ -5,9 +5,9 @@ struct InstallFontsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .frame(minWidth: 300, minHeight: 420)
+                .frame(minWidth: 500, minHeight: 300)
         }
-        .defaultSize(width: 340, height: 420)
+        .defaultSize(width: 550, height: 320)
         .windowResizability(.contentSize)
     }
 }
